7eecd8acba58ff7c604cfc57339c8fcf21a4497b
Evgeny Pimenov <evgeny-p-@users.noreply.github.com>
2017-03-04 18:19:47 +0700

    Separate elements with comma

--- main.c
+++ main.c
@@ -19,46 +19,25 @@ int min_element(int *arr, int size)
 int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
-
-    //<<<<<<< HEAD
-    //   const int min = min_element(arr, 10);
-
-//<<<<<<< c936a731ff567ac0374b3032c3b5d25023a19897
-    //    printf("Array: ");
-    //    int i;
-    //   for (i = 0; i < 10; ++i) {
-    //=======
-    const int min = min_element(arr, N_ELEMENTS(arr));
-//||||||| merged common ancestors
-  //  const int min = min_element(arr, 7);
-//=======
- //   const int min = min_element(arr, N_ELEMENTS(arr));
-//>>>>>>> Count array items with macro
-
     printf("Array: ");
-//<<<<<<< c936a731ff567ac0374b3032c3b5d25023a19897
-    //<<<<<<< HEAD
-    //   int i;
-    //   for (i = 0; i < N_ELEMENTS(arr); ++i) {
-      //>>>>>>> Count array items with macro
-    //      printf("%d ", arr[i]);
-    //=======
+    int i;
+    const int min = min_element(arr, N_ELEMENTS(arr));
+    printf("Array: ");
     int i = 0;
     printf("%d", arr[i]);
     ++i;
-    for (; i < N_ELEMENTS(arr); ++i) {
+    for (; i < N_ELEMENTS(arr); ++i) 
         printf(", %d", arr[i]);
-	//>>>>>>> Separate elements with comma
-//||||||| merged common ancestors
-   // int i;
-  //  for (i = 0; i < 7; ++i) {
-  //      printf("%d ", arr[i]);
-//=======
-    int i;
-    for (i = 0; i < N_ELEMENTS(arr); ++i) {
+    for (i = 0; i < N_ELEMENTS(arr); ++i) 
         printf("%d ", arr[i]);
-//>>>>>>> Count array items with macro
-    }
+    int i;
+    for (i = 0; i < N_ELEMENTS(arr); ++i) 
+        printf("%d ", arr[i]);
+    int i = 0;
+    printf("%d", arr[i]);
+    ++i;
+    for (; i < N_ELEMENTS(arr); ++i) 
+        printf(", %d", arr[i]);
     printf("\n");
 
     printf("Min element: %d\n", min);
